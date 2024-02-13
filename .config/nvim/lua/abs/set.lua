69837c4cde65ee5b50e42a64a0623ef4fa9cc9a7
abdullahshahzadd <shahzad097@outlook.com>
2024-02-13 11:47:21 -0500

    feat: lsp setup; feat: tmux config init

--- .config/nvim/lua/abs/set.lua
+++ .config/nvim/lua/abs/set.lua
@@ -33,7 +33,9 @@ vim.opt.colorcolumn = "80"
 
 vim.opt.guifont="DejaVuSansMNerdFontMono-Regular"
 
-vim.g.mapleader = " "
 
 vim.opt.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
+
+-- Registering templ file extension
+vim.filetype.add({ extension = { templ = "templ" } })
---
+++ .config/nvim/lua/plugins/plugin.lua
@@ -0,0 +1,9 @@
+return {
+  'mbbill/undotree',
+  'christoomey/vim-tmux-navigator',
+  {'brenoprata10/nvim-highlight-colors',
+  config = function()
+    require('nvim-highlight-colors').setup()
+  end},
+  'jiangmiao/auto-pairs',
+ }
